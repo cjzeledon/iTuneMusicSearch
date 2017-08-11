72c0ca18334ac439d0d12f876e233fd78091f74c
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-10 22:20:10 -0400

    Created const variables to querySelect from index file.

--- main.js
+++ main.js
@@ -7,3 +7,9 @@
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
+
+const player = document.querySelector(".player");
+const musicPlayer = document.querySelector(".music-player");
+const search = document.querySelector(".search");
+const searchForm = document.querySelector(".search-form");
+const results = document.querySelector(".results");
