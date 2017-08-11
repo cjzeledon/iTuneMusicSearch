d0e185263efc336d7e71174627faf347fdd6bf0c
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-11 13:06:13 -0400

    Made several changes and forgot to commit each of the changes. However, did not bother to work on CSS, not until I have the js part working the way I need it to first.

--- main.js
+++ main.js
@@ -13,3 +13,50 @@ const musicPlayer = document.querySelector(".music-player");
 const search = document.querySelector(".search");
 const searchForm = document.querySelector(".search-form");
 const results = document.querySelector(".results");
+const submitBtn = document.querySelector("#submit");
+
+let blah = " ";
+
+submitBtn.addEventListener("click",
+  function(ev) {
+    ev.preventDefault();
+    let searchSongs = document.querySelector("#FindSongs").value;
+    console.log('button works');
+      // return SearchSongs;
+      // fetch (`https://itunes.apple.com/search?term=${searchSongs}`)
+      fetch ("https://itunes.apple.com/search?term=queen")
+        .then (musicJSON)
+        .then (musicFIND)
+        .catch (catchALL);
+})
+
+function musicJSON(musicData){
+  return musicData.json();
+}
+
+function musicFIND (musicResults){
+  console.log(musicResults);
+  console.log(musicResults.results[0].trackName);
+  console.log(musicResults.results[0].artistName);
+  console.log(musicResults.results[0].artworkUrl100);
+
+  let spaceOUT = " ";
+
+  // Do not forget to add link to the thumbnail to probably play music/add music
+  for (i = 0; i < musicResults.results.length; i++){
+    let populateResults =`
+      <div class="search_giverecipes">
+      <figure>
+      <a href="${givetoAPI.results[i].href}" target="_blank">
+      ${backupImage}
+      <figcaption>${givetoAPI.results[i].title}</figcaption>
+      <figure>
+      </div>
+    `;
+  }
+
+}
+
+function catchALL (err){
+  console.log(err);
+}
